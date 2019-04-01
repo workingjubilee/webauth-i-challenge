5d73a7c021f4a9043d8610f64850c2591a28438e
workingjubilee <workingjubilee@gmail.com>
2019-04-01 14:56:06 -0700

    Users with restrict added, MVP met.

--- users/userRouter.js
+++ users/userRouter.js
@@ -6,24 +6,76 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 
-// const Users = require('./userKnex.js');
+const Users = require('./userKnex.js');
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
-  res.status(200).json({ message: "Check!" })
+router.get('/', async (req,res) => {
+  try {
+  const userList = await Users.find();
+
+  res.status(200).json(userList);
+} catch(error) {
+  res.status(500).json({ message: "Whoops!" })
+}
 });
 
 router.post('/login', (req,res) => {
 
 });
 
-router.post('/register', (req,res) => {
+router.post('/register', async (req, res) => {
+  let { name, password } = req.body;
+
+  if (!name || !password) {
+    res.status(400).json({ message: "Need both name and password to register." })
+  } else {
+    const hash = await bcrypt.hashSync(password, 4);
+    password = hash;
+
+    try {
+      const newUser = await Users.add({ name, password });
+      res.status(200).json(newUser);
+    } catch(error) {
+      res.status(500).json({ error: "Huh?" });
+  }}
 
 });
 
-router.get('/users', (req,res) => {
+router.get('/users', restrict, async (req,res) => {
+  try {
+    const userList = await Users.find();
 
+    res.status(200).json(userList);
+  } catch(error) {
+    res.status(500).json({ message: "Eh?" })
+  }
 });
+
+async function restrict(req,res,next) {
+  let { name, password } = req.headers;
+
+  if (!name || !password) {
+    res.status(401).json({ message: "Please provide credentials." })
+  } else {
+    console.log({ name, password });
+
+    try {
+      let foundUser = await Users.findByName(name);
+      console.log(foundUser);
+
+
+      if (foundUser && bcrypt.compareSync(password, foundUser.password)) {
+        next();
+      } else {
+        res.status(403).json({ message: "You shall not pass!" })
+      }
+
+    } catch(error) {
+      res.status(500).json({ message: "Error!" })
+    }
+
+}};
+
 
 module.exports = router;
